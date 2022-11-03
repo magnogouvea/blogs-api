06aadffeaf429346a300dee603c9cfb3888b7044
magnogouvea <magnogouvea@gmail.com>
2022-11-03 20:04:22 -0300

    Fix: req05

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,6 +6,6 @@ const tokenValidation = require('../middlewares/tokenValidation');
 const router = express.Router();
 
 router.post('/user', validations.userValidation, userController.user);
-router.get('/', tokenValidation.tokenValidation, userController.getUsers);
+router.get('/user', tokenValidation.tokenValidation, userController.getUsers);
 
 module.exports = router;
